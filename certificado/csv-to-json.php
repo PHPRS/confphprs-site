3db9b9e0d1b12e120dff5207ac4f1d4559ea3561
Rodrigo Gattermann <rodrigo.gattermann@unicred.com.br>
2017-05-19 17:40:09 -0300

    Ajustada hieraquia do json de participantes

--- certificado/csv-to-json.php
+++ certificado/csv-to-json.php
@@ -1,17 +1,37 @@
 <?php
 
-$id = 0;
-$events = [
-    '2ª Conferência PHPRS' => [],
-    'Workshop Segurança para aplicações web' => [],
-    'Workshop Middlewares e PSR-7' => [],
-    'Workshop Desenvolvimento de temas WordPress' => [],
-    'Workshop Docker para desenvolvedores' => []
+$events = [];
+$list_events = [
+    '2ª Conferência PHPRS',
+    'Workshop Segurança para aplicações web',
+    'Workshop Middlewares e PSR-7',
+    'Workshop Desenvolvimento de temas WordPress',
+    'Workshop Docker para desenvolvedores'
 ];
-$participants = [];
-
 $description = "participou do _event,\ncumprindo um total de _hours horas de\n palestras e demais atividades ofertadas\n no dia _date.";
 
+foreach ($list_events as $key => $str_event) {
+    $hours = 8;
+    $date = '13 de maio de 2017';
+
+    if(strpos($str_event, 'Workshop')) {
+        $hours = 3;
+        $date = '12 de maio de 2017';
+    }
+
+    $subs = array('_event', '_hours', '_date');
+    $values = array($str_event, $hours, $date);
+    $new_description = str_replace($subs, $values, $description);
+
+    $events[$str_event] = [
+        'id' => $key+1,
+        'name' => $str_event,
+        'description' => $new_description,
+        'attendee' => []
+    ];
+}
+
+$id = 0;
 if (($handle = fopen('participant.csv', 'r')) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
         $id ++;
@@ -19,40 +39,23 @@ if (($handle = fopen('participant.csv', 'r')) !== FALSE) {
         $email = $data[1];
         $type_inscription = $data[2];
 
-        $hours = 8;
-        $date = '13 de maio de 2017';
-
-        if(strpos($type_inscription, 'Workshop')) {
-            $hours = 3;
-            $date = '12 de maio de 2017';
-        }
-
-        $subs = array('_event', '_hours', '_date');
-        $values = array($type_inscription, $hours, $date);
-        $new_description = str_replace($subs, $values, $description);
-
         $guy = [
             'id' => $id,
-            'name' => $type_inscription,
-            'description' => $new_description,
-            'attendee' => [
-                'id' => 1,
-                'name' => $name,
-                'file' => '',
-                'type' => 'attendee',
-                'bg_file' => '',
-                'data' => "{$hours} hours",
-                'email' => $email
-            ]
+            'name' => $name,
+            'file' => '',
+            'type' => 'attendee',
+            'bg_file' => '',
+            'data' => "{$hours} hours",
+            'email' => $email
         ];
-        $events[$type_inscription][] = $guy;
+        $events[$type_inscription]['attendee'][] = $guy;
     }
     fclose($handle);
 
-    $participants['event'] = $events;
-
     $fp = fopen('participant-2017.json', 'w');
-    fwrite($fp, json_encode($participants));
+    $new_events = array_values($events);
+    arsort($new_events);
+    fwrite($fp, json_encode(['event'=>$new_events]));
     fclose($fp);
     echo 'json criado';
 }
